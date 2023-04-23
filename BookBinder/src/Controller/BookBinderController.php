3dcb5d9d47d91a755f943d01fc02821be653f641
Cornelia Morissa <corneliamorissa.untung@student.kul>
2023-04-24 00:31:25 +0200

    adding base twig and html css for home page

---
+++ BookBinder/src/Controller/BookBinderController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookBinderController extends AbstractController
+{
+    private array $stylesheets;
+
+    public function __construct() {
+        $this->stylesheets[] = 'main.css';
+    }
+
+    /**
+     * @Route("/Home", name="Home")
+     */
+    #[Route("/Home", name: "Home")]
+    public function home(): Response {
+        return $this->render('home.html.twig', [
+            'stylesheets' => $this->stylesheets
+        ]);
+    }
+
+    /**
+     * @Route("/Home", name="Home")
+     */
+    #[Route("/Search", name: "Search")]
+    public function search(): Response {
+        return $this->render('home.html.twig', [
+            'stylesheets' => $this->stylesheets
+        ]);
+    }
+
+    /**
+     * @Route("/Home", name="Home")
+     */
+    #[Route("/MeetUp", name: "MeetUp")]
+    public function meetup(): Response {
+        return $this->render('home.html.twig', [
+            'stylesheets' => $this->stylesheets
+        ]);
+    }
+
+    /**
+     * @Route("/Home", name="Home")
+     */
+    #[Route("/User", name: "User")]
+    public function user(): Response {
+        return $this->render('home.html.twig', [
+            'stylesheets' => $this->stylesheets
+        ]);
+    }
+}
