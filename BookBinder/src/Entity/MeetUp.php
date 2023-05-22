96f0fd0b413bf868bfafb0b7369a60b489d17592
joerivc <joeri.vancauwelaert@student.kuleuven.be>
2023-05-22 14:29:02 +0200

    adjustment 2.0

--- BookBinder/src/Entity/MeetUp.php
+++ BookBinder/src/Entity/MeetUp.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Entity;
-use App\Repository\LoginUserRepository;
+use App\Repository\MeetUpRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MeetUpRepository :: class)]
