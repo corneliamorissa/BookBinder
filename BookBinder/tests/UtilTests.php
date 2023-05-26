543fa1cc2982db17f4e245e54da25d504080d7fa
Cornelia Morissa <corneliamorissa.untung@student.kul>
2023-05-26 16:34:49 +0200

    test coverage 4%

--- BookBinder/tests/UtilTests.php
+++ BookBinder/tests/UtilTests.php
@@ -6,12 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UtilTests extends WebTestCase
 {
-    public function testSomething(): void
+    public function testRouteHome(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('#login_top_trending', 'Top Trending Books');
+        /*$trendingbook = $crawler->filter('#trendingBook');
+        $this->assertEquals(3, $trendingbook->count());*/
     }
 }
