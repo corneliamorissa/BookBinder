160b9ee52a9e4b08203b767eb37256c02af2f62a
joerivc <joeri.vancauwelaert@student.kuleuven.be>
2023-06-07 14:03:50 +0200

    changed MeetupFixtures.php 1.4

--- BookBinder/src/DataFixtures/UserBookFixtures.php
+++ BookBinder/src/DataFixtures/UserBookFixtures.php
@@ -12,7 +12,7 @@ class UserBookFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 // open csv file and load data
-        if (($handle = fopen(__DIR__."/data/libraries.csv", "r")) !== FALSE) {
+        if (($handle = fopen(__DIR__."/data/books.csv", "r")) !== FALSE) {
             // read first line with headers
             $headers = fgetcsv($handle, 1000, ",");
             // read rest of file and create entities for every line
