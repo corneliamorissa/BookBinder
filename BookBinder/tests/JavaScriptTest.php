a21e259330c7503572c58c0733c4a1e93cdfc636
joerivc <joeri.vancauwelaert@student.kuleuven.be>
2023-06-08 09:21:49 +0200

    changed it back to JavaScriptTest.php

--- BookBinder/tests/JavaScriptTest.php
+++ BookBinder/tests/JavaScriptTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\AssertionFailedError;
 use Symfony\Component\Panther\PantherTestCase;
 use Symfony\Component\Panther\Client;
 //when testing check if chrome driver is running in background, if it is, end the task in task manager, and run test again
-class ZavaScriptTest extends PantherTestCase
+class JavaScriptTest extends PantherTestCase
 {
     /**
      * @throws NoSuchElementException
